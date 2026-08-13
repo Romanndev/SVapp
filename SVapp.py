5096443ccd27bfa571cc5013acd57ea0bf54faed
Romanndev <new4rch@gmail.com>
2026-08-12 20:50:13 -0600

    update

--- SVapp.py
+++ SVapp.py
@@ -91,4 +91,4 @@ def get_ticker(id: int):
         
 # ---------------------------------------------------------------------
 if __name__ == "__main__":
-    uvicorn.run("test_db:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("SVapp:app", host="127.0.0.1", port=8000, reload=True)
