3bd992cbd33397f775972ca6eacc2a884d906ad3
Romanndev <new4rch@gmail.com>
2026-09-12 18:48:28 -0600

    Update SVapp.py

--- SVapp.py
+++ SVapp.py
@@ -35,23 +35,23 @@ def ticker_info(ticker:str):
         return row
 
 # добавление нового тикера
-@app.post("/ticker/add_ticker/{ticker}", response_model=schemas.ticker_info | dict[str,str])
-def add_ticker(ticker:str):
-        ticker = db.check_ticker_name(ticker)
-        date_for_DB = sv.newticker_date(ticker) 
-        if date_for_DB.fullname == 'no date': 
-            return {'status':'Ticker data not found'}
+# @app.post("/ticker/add_ticker/{ticker}", response_model=schemas.ticker_info | dict[str,str])
+# def add_ticker(ticker:str):
+#         ticker = db.check_ticker_name(ticker)
+#         date_for_DB = sv.newticker_date(ticker) 
+#         if date_for_DB.fullname == 'no date': 
+#             return {'status':'Ticker data not found'}
         
-        with db.get_db_connection() as conn, conn.cursor() as cur:
-            row = db.save_new_ticker(cur, date_for_DB)
-        return row
+#         with db.get_db_connection() as conn, conn.cursor() as cur:
+#             row = db.save_new_ticker(cur, date_for_DB)
+#         return row
  
 # удаление тикера из БД
-@app.delete("/ticker/delete_ticker/{ticker}")
-def delete_ticker(ticker:str):
-    with db.get_db_connection() as conn,conn.cursor() as cur:
-        if not db.ticker_info(cur, ticker): return {'status':'ticker not found'}
-        if db.delete_record(cur,ticker) == True: return  {'status': 'ticker is deleted'}
+# @app.delete("/ticker/delete_ticker/{ticker}")
+# def delete_ticker(ticker:str):
+#     with db.get_db_connection() as conn,conn.cursor() as cur:
+#         if not db.ticker_info(cur, ticker): return {'status':'ticker not found'}
+#         if db.delete_record(cur,ticker) == True: return  {'status': 'ticker is deleted'}
 
 # обновление данных по всем тикерам в БД 
 # @app.patch("/ticker/update_tickers", response_model=dict[str,str]) 
